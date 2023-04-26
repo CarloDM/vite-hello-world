95489679e3fbfb2740a2316d043048cc55791bfb
carloDM <carlo.de.mauro.work@gmail.com>
2023-04-26 19:55:57 +0200

    appVue

--- vite-hello-world/src/App.vue
+++ vite-hello-world/src/App.vue
@@ -1,30 +1,27 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
+<script>
+export default {
+  name: 'app',
+  data(){
+    return{
+      message :'ciao Vite!'
+    }
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="init" />
+  <h1>{{ message }}</h1>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+h1{
+  padding: 20px;
+  color: black;
+  background-color: rgb(209, 209, 209);
+  border: 2px solid red;
+  border-radius: 20px;
+  line-height: 100%;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
