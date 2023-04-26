8a59e257ff5cbd681a5d43c45c01ffc6ac720370
carloDM <carlo.de.mauro.work@gmail.com>
2023-04-26 20:10:44 +0200

    add component

---
+++ vite-hello-world/src/components/appTitle.vue
@@ -0,0 +1,21 @@
+
+<script>
+export default {
+  name: 'appTitle',
+  data( ){
+    return {
+      message:'La mia prima app con Vite'
+    }
+  }
+}
+</script>
+
+<template>
+  <p>{{ message }}</p>
+</template>
+<style scoped>
+p{
+  color: white;
+}
+
+</style>
